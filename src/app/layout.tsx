1095f474b7d8e2f162eb80181f9bfd72397bda14
Javier Lim <javierlimt6@gmail.com>
2025-07-13 01:07:36 +0800

    add speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const inter = Inter({
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         {children}
         <Analytics />
+        <SpeedInsights />
         </body>
     </html>
   );
