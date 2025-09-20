d1d0fa68e306123bde8999563199d1cf8a6de4cb
Javier Lim <javierlimt6@gmail.com>
2025-09-20 15:43:47 +0800

    add redirect from /blazeup-social to /blazeup-social.pdf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/blazeup-social",
+        destination: "/blazeup-social.pdf",
+        permanent: true, // 301 redirect; set to false if temporary (302)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
